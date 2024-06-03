6a00e58629b859dc347e759405d965dbd2afbb15
David Bruce <bruce.j.david@gmail.com>
2024-06-02 23:12:55 -0400

    Adds 404.html and redirect script from https://github.com/rafgraph/spa-github-pages

--- publish-gh-page.sh
+++ publish-gh-page.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 dotnet fable --run npx vite build
+cp 404.html dist/
 git add dist && git commit -m "Publish $(date +"%T")"
 git push origin `git subtree split --prefix dist main`:gh-pages --force
 git reset HEAD~
