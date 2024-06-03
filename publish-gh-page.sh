87c275ce1b7c603075dfee64e16f1cd2e93fa71d
David Bruce <bruce.j.david@gmail.com>
2024-06-02 22:46:22 -0400

    Publish 22:46:22

--- publish-gh-page.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-dotnet fable --run npx vite build
-git add dist && git commit -m "Publish $(date +"%T")"
-git push origin `git subtree split --prefix dist main`:gh-pages --force
-git reset HEAD~
-rm -rf dist
